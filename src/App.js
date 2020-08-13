d42752d396f17d38d45c098f83182b5a5a724d2c
Sean de Groot <degroot.sd@gmail.com>
2020-08-13 17:34:05 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import './index.scss';
 
 function App() {
+
+  const [userName, getUsername] = useState('');
+
+  const User = () => {
+    getUsername('doug');
+    return (
+      <div>
+        <p>Hello {userName}</p>
+      </div>
+    )
+  }
+
+  {/* redirect to /UserInfo, with the userName value being a param propped into UserInfo */ }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="wrapper">
+        <header>
+          <h1>Git Check</h1>
+          <Route exact path="/">
+            <form onSubmit={handleSubmit}>
+              <label className="sr-only" htmlFor="username">Enter your username</label>
+              <input
+                name="username"
+                type="text"
+                value={userName}
+                onChange={e => getUsername(e.target.value)}
+              />
+            </form>
+          </Route>
+        </header>
+        <Route exact path="/user" component={User} />
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
